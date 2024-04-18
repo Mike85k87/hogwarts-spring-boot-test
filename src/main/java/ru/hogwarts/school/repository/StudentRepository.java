6f40866e1994f6642acccc0c46cb428dd21ee2be
mike85k87 <mike85k@yandex.ru>
2024-04-18 16:32:04 +0700

    Переделал тесты

---
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package ru.hogwarts.school.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    // Collection<Student> findByAge(int age);
+
+    List<Student> findAllByAgeBetween(Integer minAge, Integer maxAge);
+}
